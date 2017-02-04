29ce27559814923d35ae961ae505035a0464e10e
linziyanleo <linziyanleo@users.noreply.github.com>
2017-02-03 20:14:45 -0800

    Add files via upload

--- picEncrypt.py
+++ picEncrypt.py
@@ -3,20 +3,25 @@ from PIL import Image
 
 class Cube:
     def __init__(self):
-        self.im = Image.open('cat.jpg')
+        self.im = Image.open('ucsb.jpeg')
         #self.imenc = im
-        self.cubeSize = 8
+        self.cubeSize = 50
         self.size = self.im.size
     def Row(self):
         self.loop1 = self.size[0]
         self.loop2 = self.size[1]
         x = 0
-        y = (rowLine-1)*self.cubeSize
-        
-        for i in range(0,(self.size[0]//self.cubeSize)-1):
-            #for k in range(0,(self.size[1]//self.cubeSize)-1):
-            x += i*self.cubeSize
-            box = (x,x,y,y)
-            box2 = (x+8,x+8,y,y)
-            region = im.crop(box)
-            im.paste(region, box2)
+        y = 0
+		#for i in range(0,(self.size[0]//self.cubeSize)-2):
+		#for k in range(0,(self.size[1]//self.cubeSize)-1):
+	    x += self.cubeSize			
+        box = (x-self.cubeSize , y , x , y+self.cubeSize)
+	    leftbox = (x , y , self.loop1 , y+self.cubeSize)
+    	box2 = (x+self.cubeSize , y , x+2*self.cubeSize , y+self.cubeSize)
+    	self.region = self.im.crop(box)
+    	self.region.load()
+    	self.im.paste(self.region, box2)
+#self.im.show()
+c = Cube()
+c.Row()
+c.im.show()	
